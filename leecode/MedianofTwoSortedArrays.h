754d260136796e277e658c318250c8ff3f747d28
chailyuan-notebook <chailyuan2@gmail.com>
2016-11-24 17:38:48 +0800

    1

---
+++ leecode/MedianofTwoSortedArrays.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class MedianofTwoSortedArrays {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		
+		return 0;
+	}
+};
--- leecode/main.cpp
+++ leecode/main.cpp
@@ -1,17 +1,8 @@
 #include "ReverseInteger.h"
-
+#include "MedianofTwoSortedArrays.h"
 
 int main()
 {
-	int a,b;
-	a = 
-		-2147483412
-		//		1463847412
-		//		0
-		//		1
-		//		-1
-		;
-	ReverseInteger s;
-	b=s.reverse(a);
+	
 	return 0;
 }
